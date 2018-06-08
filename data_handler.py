fc24a005e742d2440cf40827ed0a588f9a876dfa
Tim Coutinho <tmcoutinho42@gmail.com>
2018-06-07 23:09:50 -0700

    Added separate file for handling data, would recommend using

---
+++ data_handler.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def train_test_split(train_file, test_file):
+    train_data = pd.read_csv('data/train/' + train_file)
+    test_data = pd.read_csv('data/test/' + test_file)
+    uni = train_data['UniversityApplied'][0]
+    X_train = train_data[['GRE', 'GRE-V', 'GRE (Quants)', 'AWA', 'TOEFL',
+                          'Work-Ex', 'International Papers', 'Percentage']]
+    y_train = pd.get_dummies(train_data['Result'])['Accept']
+    X_test = test_data.drop(['Result'], axis=1)
+    y_test = pd.get_dummies(test_data['Result'])['Accept']
+    return uni, X_train, y_train, X_test, y_test
